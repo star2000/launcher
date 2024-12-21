8fb40e43ff5a2e5fb9a649b5d8c7ed4a9a9ca7ac
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-12-21 21:23:48 +1100

    docs: add comments to renderer ipc

--- src/renderer/services/ipc.ts
+++ src/renderer/services/ipc.ts
@@ -2,5 +2,11 @@ import { IpcEmitter, IpcListener } from '@electron-toolkit/typed-ipc/renderer';
 
 import type { IpcEvents, IpcRendererEvents } from '@/shared/types';
 
+/**
+ * A typed IPC listener for the renderer process.
+ */
 export const ipc = new IpcListener<IpcRendererEvents>();
+/**
+ * A typed IPC emitter for the renderer process.
+ */
 export const emitter = new IpcEmitter<IpcEvents>();
