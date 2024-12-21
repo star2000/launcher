39e06d192a9573aa3b7460f4189ade0b8e1a920d
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-12-21 20:58:55 +1100

    docs: add comments for StringMatcher

--- src/lib/string-matcher.ts
+++ src/lib/string-matcher.ts
@@ -1,3 +1,10 @@
+/**
+ * A class that can be used to match a string against a regular expression and execute a callback when a match is found.
+ *
+ * This class is useful for parsing logs or other streams of text data and extracting URLs or other information.
+ *
+ * The `filter` function is used to determine if the data should be checked for a match as a performance optimization.
+ */
 export class StringMatcher {
   private matched: boolean;
   private onMatch: (url: string) => void;
@@ -11,6 +18,12 @@ export class StringMatcher {
     this.re = arg.re;
   }
 
+  /**
+   * Checks the data for a match against the regular expression. If a match is found, the `onMatch` callback is executed.
+   *
+   * Once a match is handled, the `matched` property is set to true and no further matches will be checked. To continue
+   * checking for matches, call the `reset` method in your `onMatch` callback.
+   */
   checkForMatch = (data: string) => {
     if (this.matched) {
       return;
@@ -31,10 +44,16 @@ export class StringMatcher {
     this.onMatch(url);
   };
 
+  /**
+   * Resets the matcher so that it can be used to match again.
+   */
   reset = () => {
     this.matched = false;
   };
 
+  /**
+   * Returns true if a match has been found. This is reset to false when the `reset` method is called.
+   */
   hasMatched = () => {
     return this.matched;
   };
